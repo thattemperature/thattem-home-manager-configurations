ec323ebd1475ff3ba2a94e2613003463927aa04a
thattemperature <2719023332@qq.com>
2026-06-14 17:31:17 +0800

    Add firefox profile settings.

--- functions/browser/firefox.nix
+++ functions/browser/firefox.nix
@@ -15,6 +15,21 @@
         nativeMessagingHosts = [
           pkgs.gnome-browser-connector
         ];
+        profiles = {
+          thattemperature-default = {
+            isDefault = true;
+            settings = {
+              "browser.download.useDownloadDir" = false;
+              "browser.startup.page" = 3;
+              "media.autoplay.default" = 0;
+              "signon.autofillForms" = false;
+              "signon.generation.enabled" = false;
+              "signon.management.page.breach-alerts.enabled" = false;
+              "signon.rememberSignons" = false;
+              "toolkit.tabbox.switchByScrolling" = true;
+            };
+          };
+        };
       };
     }
   );
