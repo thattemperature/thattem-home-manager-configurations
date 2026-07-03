5982bbc296c05605a6e28ca5867a993a41102ca4
thattemperature <2719023332@qq.com>
2026-07-03 22:50:15 +0800

    Add "thattem-gnome-extension".

--- functions/desktop/gnome/extensions.nix
+++ functions/desktop/gnome/extensions.nix
@@ -30,6 +30,7 @@
         { package = pkgs.gnomeExtensions.extension-list; }
         { package = pkgs.gnomeExtensions.open-bar; }
         { package = pkgs.gnomeExtensions.system-monitor; }
+        { package = pkgs.gnomeExtensions.thattem-gnome-extension; }
         { package = pkgs.gnomeExtensions.tiling-assistant; }
         { package = pkgs.gnomeExtensions.top-bar-organizer; }
         { package = pkgs.gnomeExtensions.transparent-window-moving; }
