b6eb067362ad3b24ab1b286e10aea862d749c1c9
thattemperature <2719023332@qq.com>
2026-07-05 13:28:39 +0800

    Update "flake.lock" and move the override to the overlays repository.

--- functions/programming/nix.nix
+++ functions/programming/nix.nix
@@ -11,18 +11,7 @@
     lib.mkIf config.thattem.home-manager.programming.enable {
 
       home.packages = with pkgs; [
-        # Workaround: update temporarily
-        (haskell.lib.overrideCabal haskellPackages.dconf2nix (prev: {
-          src = pkgs.fetchFromGitHub {
-            owner = "thattemperature";
-            repo = "dconf2nix";
-            rev = "dd1dacf17ed97be48d459e6c72524872fd0e6ab6";
-            hash = "sha256-P2jxSiq5aypnhk+J0W5dOqsoyjIX9l8Uz2xeRXBuSCc=";
-          };
-          libraryHaskellDepends = prev.libraryHaskellDepends ++ [
-            pkgs.haskellPackages.utf8-string
-          ];
-        }))
+        dconf2nix
         nixd
         nixfmt
         nix-prefetch-github
