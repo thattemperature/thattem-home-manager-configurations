5316fbb094b3d2427fce6f9c5e95071809257652
thattemperature <2719023332@qq.com>
2026-08-04 20:17:01 +0800

    Fix little bug.

--- functions/desktop/gnome/interface.nix
+++ functions/desktop/gnome/interface.nix
@@ -14,7 +14,7 @@ with lib.hm.gvariant;
           clock-show-seconds = true;
           clock-show-weekday = true;
           document-font-name = "Sarasa UI SC 16";
-          font-name = "Sarasa UI SC 16";
+          font-name = "Sarasa Gothic SC 16";
           gtk-enable-primary-paste = true;
           monospace-font-name = "Sarasa Mono SC 16";
         };
